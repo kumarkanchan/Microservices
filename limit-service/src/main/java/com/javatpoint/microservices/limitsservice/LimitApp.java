838bfa5860b6e0ed930da15a3c58f3c4985e7a2a
Kanchan Kumar <66953377+kumarkanchan@users.noreply.github.com>
2020-07-07 18:14:44 +0530

    This is First Commit.

---
+++ limit-service/src/main/java/com/javatpoint/microservices/limitsservice/LimitApp.java
@@ -0,0 +1,12 @@
+package com.javatpoint.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitApp {
+	public static void main(String[] args) {
+		SpringApplication.run(LimitApp.class, args);
+	}
+
+}
